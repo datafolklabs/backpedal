001823a70ceb16face638d56fed38e3984ae1d18
BJ Dierkes <derks@datafolklabs.com>
2018-02-02 13:56:28 -0600

    Remove OSX Tests (Broken)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ The primary use case for Backpedal is finding files from within your current dir
 """
 
 setup(name='backpedal',
-    version='0.9.6',
+    version='0.9.8',
     description="Backpedal",
     long_description=LONG,
     author='BJ Dierkes',
